1effcd39f93311c84fe7a362a6f3a3d56c8ca79a
Muni <muni2explore@gmail.com>
2024-10-28 08:33:52 +0530

    Removed node_modules

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: {
+    'postcss-import': {},
     tailwindcss: {},
     autoprefixer: {},
-  },
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          cssnano: {
+            preset: 'default',
+          },
+        }
+      : {})
+  }
 }
